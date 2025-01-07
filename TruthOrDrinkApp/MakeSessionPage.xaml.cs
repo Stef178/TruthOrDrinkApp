787c42ecca75fd2b71e05d856c893f82ddb02de9
Stef Konings <2302771konings@zuyd.nl>
2025-01-07 11:54:39 +0100

    Alle data schermen werkend

--- TruthOrDrinkApp/MakeSessionPage.xaml.cs
+++ TruthOrDrinkApp/MakeSessionPage.xaml.cs
@@ -1,14 +1,27 @@
-namespace TruthOrDrinkApp;
+using Microsoft.Maui.Controls;
+using System;
 
-public partial class MakeSessionPage : ContentPage
+namespace TruthOrDrinkApp
 {
-	public MakeSessionPage()
-	{
-		InitializeComponent();
-	}
-
-    private async void OnNextButtonClicked(object sender, EventArgs e)
+    public partial class MakeSessionPage : ContentPage
     {
-        await Navigation.PushAsync(new FillQuestionPool());
+        private readonly dynamic _selectedData;
+
+        public MakeSessionPage(dynamic selectedData)
+        {
+            InitializeComponent();
+            _selectedData = selectedData;
+
+            // Toon de geselecteerde gegevens in de labels
+            DaringLevelLabel.Text = $"Gewaagdheidsniveau: {_selectedData.DaringLevel}";
+            CategoriesLabel.Text = "Geselecteerde categorieën: " + string.Join(", ", _selectedData.SelectedCategories);
+            QuestionTypeLabel.Text = $"Gepersonaliseerd: {_selectedData.Personalized}, Voorgestelde: {_selectedData.Suggested}";
+        }
+
+        private async void OnNextButtonClicked(object sender, EventArgs e)
+        {
+            // Hier kun je de logica voor de QR-code toevoegen
+            await Navigation.PushAsync(new FillQuestionPool());
+        }
     }
 }
--- TruthOrDrinkApp/SelectQuestionType.xaml.cs
+++ TruthOrDrinkApp/SelectQuestionType.xaml.cs
@@ -13,10 +13,25 @@ namespace TruthOrDrinkApp
             InitializeComponent();
             _daringLevel = daringLevel;
             _selectedCategories = selectedCategories;
+        }
 
-            // Debugging: Toon de ontvangen gegevens
-            Console.WriteLine($"Gewaagdheidsniveau: {_daringLevel}");
-            Console.WriteLine("Categorieën: " + string.Join(", ", _selectedCategories));
+        private async void OnNextButtonClicked(object sender, EventArgs e)
+        {
+            // Haal de geselecteerde vraagtypes op
+            var personalizedSelected = PersonalizedCheckBox.IsChecked;
+            var suggestedSelected = SuggestedCheckBox.IsChecked;
+
+            // Maak een object van de geselecteerde data
+            var selectedData = new
+            {
+                DaringLevel = _daringLevel,
+                SelectedCategories = _selectedCategories,
+                Personalized = personalizedSelected,
+                Suggested = suggestedSelected
+            };
+
+            // Navigeren naar MakeSessionPage en data doorgeven
+            await Navigation.PushAsync(new MakeSessionPage(selectedData));
         }
     }
 }
